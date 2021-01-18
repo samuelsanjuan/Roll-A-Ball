5743e9c8dd82d517df1551f06bf22326d4c328a4
Samuel <ssanjuanandres@danielcastelao.org>
2021-01-18 10:51:26 +0100

    enemigo añadido

---
+++ Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private UnityEngine.AI.NavMeshAgent pathFinder;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    pathFinder= GetComponent<UnityEngine.AI.NavMeshAgent>();
+    target= GameObject.Find("Player").transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pathFinder.SetDestination(target.position);
+    }
+}
