ba194f9be923cdf9b472c286b34407bb88dae565
Samuel <ssanjuanandres@danielcastelao.org>
2021-01-20 11:47:02 +0100

    puntos y respawn

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject looseText;
+    public GameObject winText;
+    public TextMeshProUGUI countText;
     public float speed = 10;
+    public GameObject player;
+    private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public int monedas;
 
     void Start()
     {
         rb =GetComponent<Rigidbody>();
+        count=0;
+        monedas=9;
+
+        SetCountText();
+        winText.SetActive(false);
+        looseText.SetActive(false);
     }
 
     void OnMove(InputValue movementValue){
@@ -21,6 +34,21 @@ public class PlayerController : MonoBehaviour
 
         movementX=movementVector.x;
         movementY=movementVector.y;
+
+    }
+
+    void SetCountText(){
+
+        countText.text= "Puntiacion: "+count.ToString();
+
+        if (monedas+count<0){
+            looseText.SetActive(true);
+            winText.SetActive(false);
+        }
+
+        if (monedas<=0&&monedas+count>0){
+            winText.SetActive(true);
+        }
 
     }
 
@@ -34,7 +62,21 @@ public class PlayerController : MonoBehaviour
 
         if(other.gameObject.CompareTag("PickUp")){
             other.gameObject.SetActive(false);
-        }
+            count=count+1;
+            monedas=monedas-1;
+            SetCountText();
+        } 
 
+    }
+
+    void OnCollisionEnter(Collision collision){
+    
+        if (collision.gameObject.CompareTag("Enemy")){
+
+            player.transform.position=new Vector3(0,3,0);
+
+            count=count-1;
+            SetCountText();
+        }
     }
 }
